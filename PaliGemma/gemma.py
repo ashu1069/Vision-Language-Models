9f26be2e279eeda971e2cfc9db01eb0adedc7e1f
Ashutosh Kumar <ak1825@rit.edu>
2025-11-19 14:27:08 -0500

    paligemma skeleton implemented, and partial Gemma implementation

--- PaliGemma/gemma.py
+++ PaliGemma/gemma.py
@@ -1,17 +1,124 @@
 import torch
 from torch import nn
 from typing import Optional, Tuple, List
-from torch.nn import CrossEntropyLoss
-import math
-from siglip import SiglipVisionConfig, SiglipVisionModel
 
-class PaliGemmaForConditionalGeneration(nn.Module):
-    def __init__(self, config: PaliGemmaConfig):
-    super().__init__()
-    self.config = config
-    self.vision_tower = SiglipVisionModel(config.vision_config)
-    self.multi_modal_projector = PaliGemmaMultiModalProjector(config)
-    self.vocab_size = config.vocab_size
+class GemmaConfig():
+    def __init__(
+        self,
+        vocab_size, 
+        hidden_size,
+        intermediate_size,
+        num_hidden_layers,
+        num_attention_layers,
+        num_attention_heads,
+        num_key_value_heads,
+        head_dim = 256,
+        max_position_embeddings = 8192,
+        rms_norm_eps = 1e-6,
+        rope_theta = 10000.0,
+        attention_bias = False,
+        attention_dropout = 0.0,
+        pad_token_id = None,
+        **kwargs,
+    ):
+        super().__init__()
+        self.vocab_size = vocab_size
+        self.hidden_size = hidden_size
+        self.intermediate_size = intermediate_size
+        self.num_hidden_layers = num_hidden_layers
+        self.num_key_value_heads = num_key_value_heads
+        self.head_dim = head_dim
+        self.max_position_embeddings = max_position_embeddings
+        self.rms_norm_eps = rms_norm_eps
+        self.num_attention_heads = num_attention_heads
+        self.rope_theta = rope_theta
+        self.attention_bias = attention_bias
+        self.attention_dropout = attention_dropout
+        self.pad_token_id = pad_token_id
 
-    language_model = GemmaForCausalLM(config.text_config)
-    self.language_model = language_model
+class GemmaModel(nn.Module):
+    def __init__(self, config: GemmaConfig):
+        super().__init__()
+        self.config = config
+        self.padding_idx = config.pad_token_id
+        self.vocab_size = config.vocab_size
+
+        self.embed_tokens = nn.Embedding(config.vocab_size, config.hidden_size, self.padding_idx)
+        self.layers = nn.ModuleList(
+            [GemmaDecoderLayer(config, layer_idx) for layer_idx in range(config.num_hidden_layers)]
+        )
+        self.norm = GemmaRMSNorm(config.hidden_size, eps=config.rms_norm_eps)
+
+    def get_input_embeddings(self):
+        return self.embed_tokens
+
+    def forward(
+        self,
+        attention_mask: Optional[torch.Tensor] = None,
+        position_ids: Optional[torch.LongTensor] = None,
+        inputs_embeds: Optional[torch.FloatTensor] = None,
+        kv_cache: Optional[KVCache] = None,
+    ) -> torch.FloatTensor:
+
+        hidden_states = inputs_embeds
+
+        normalizer = torch.tensor(self.config.hidden_size**0.5, dtype=hidden_states.dtype, device=hidden_states.device)
+
+        hidden_states = hidden_states * normalizer
+
+        for decode_layer in self.layers:
+            # [B, seq_len, hidden_size]
+            hidden_states = decoder_layer(
+                hidden_states,
+                attention_mask=attention_mask,
+                position_ids = position_ids,
+                kv_cache=kv_cache
+            )
+
+        hidden_states = self.norm(hidden_states)
+
+        return hidden_states
+
+class GemmaForCausalLM(nn.Module):
+    def __init__(self, config: GemmaConfig):
+        super().__init__()
+        self.config = config
+        self.model = GemmaModel(config)
+        self.vocab_size = config.vocab_size
+        self.lm_head = nn.Linear(config.hidden_size, config.vocab_size, bias=False)
+
+    def get_input_embeddings(self):
+        return self.model.embed_tokens
+
+    def tie_weights(self):
+        self.lm_head.weight = self.model.embed_tokens.weight
+
+    def forward(
+        self,
+        attention_mask: Optional[torch.Tensor] = None,
+        position_ids: Optional[torch.LongTensor] = None,
+        inputs_embeds: Optional[torch.FloatTensor] = None,
+        kv_cache: Optional[KVCache] = None,
+    ) -> Tuple:
+
+        # inputs_embeds: [B, seq_len, hidden_size]
+        # outputs: [B, seq_len, hidden_size]
+        outputs = self.model(
+            attention_mask = attention_mask,
+            position_ids = position_ids,
+            inputs_embeds = inputs_embeds,
+            kv_cache = kv_cache,
+        )
+
+        hidden_states = outputs
+        logits = self.lm_head(hidden_states)
+        logits = logits.float()
+
+        return_data = {
+            'logits': logits,
+        }
+        if kv_cache is not None:
+            # return the updated cache
+            return_data["kv_cache"] = kv_cache
+
+        return return_data
